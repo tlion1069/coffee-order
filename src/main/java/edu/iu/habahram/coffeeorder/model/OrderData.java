c52ceb40fec8c83408f52b4824179c08f2591b51
jun-haolei <junhlei@iu.edu>
2024-04-26 23:54:08 -0400

    HW9 Part 3

--- src/main/java/edu/iu/habahram/coffeeorder/model/OrderData.java
+++ src/main/java/edu/iu/habahram/coffeeorder/model/OrderData.java
@@ -1,36 +1,5 @@
 package edu.iu.habahram.coffeeorder.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import org.aspectj.weaver.ast.Or;
-
 import java.util.List;
-@Entity
-@Table(schema = "coffee")
-public class OrderData {
-    @Id
-    private String beverage;
-    private List<String> condiments;
 
-    public String getBeverage() {
-        return beverage;
-    }
-
-    public OrderData(String beverage, List<String> condiments) {
-        this.beverage = beverage;
-        this.condiments = condiments;
-    }
-
-    public void setBeverage(String beverage) {
-        this.beverage = beverage;
-    }
-
-    public List<String> getCondiments() {
-        return condiments;
-    }
-
-    public void setCondiments(List<String> condiments) {
-        this.condiments = condiments;
-    }
-    public OrderData(){}
+public record OrderData(String beverage, List<String> condiments) {
 }
